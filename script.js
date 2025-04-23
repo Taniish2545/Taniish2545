1d19257ec658b086d247a70cd8be32d7865d80b3
Taniish2545 <tanishpatel0425@gmail.com>
2025-04-23 16:12:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Example interactive functionality
+window.onload = function() {
+    console.log("Website loaded successfully.");
+
+    // Add more interactive functions here if needed later
+};

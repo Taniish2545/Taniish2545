8ec595fb3f392968c0bd533c0428efc4fdcd4f41
Taniish2545 <tanishpatel0425@gmail.com>
2025-04-23 23:28:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Example interactive functionality
-window.onload = function() {
-    console.log("Website loaded successfully.");
-};
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('mobile-menu');
     const navList = document.getElementById('navList');
   
-    menuToggle.addEventListener('click', () => {
-      navList.classList.toggle('show');
-    });
+    if (menuToggle && navList) {
+      menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('show');
+      });
+    }
   });
